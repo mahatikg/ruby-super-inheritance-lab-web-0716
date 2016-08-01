5e6d6adfbb6b97618777b9cf7069d9f59113a053
Mahati Gollamudi <github email address>
2016-07-31 23:05:10 -0400

    Super Inheritance Lab Complete

---
+++ lib/chatty_student.rb
@@ -0,0 +1,25 @@
+class ChattyStudent < Student
+
+  def hello
+    super
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+  end
+
+  def raise_hand
+    #10.times do |super|
+    #why can't we do something like this?
+    #i tried various versions and they all failed 
+
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+  end
+
+end
